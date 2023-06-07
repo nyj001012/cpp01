cfd4b4714648d236818a7918f90d149099e2ed61
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-07 21:10:25 +0900

    feat: implement Weapon class

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,15 +6,25 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 20:37:53 by yena              #+#    #+#             */
-/*   Updated: 2023/06/07 20:38:57 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/07 21:09:54 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CPP01_EX03_WEAPON_H_
-#define CPP01_EX03_WEAPON_H_
+#ifndef CPP01_EX03_WEAPON_HPP_
+#define CPP01_EX03_WEAPON_HPP_
 
+/**
+ * @brief Weapon class
+ */
 class Weapon {
+ private:
+    const std::string _type; ///< const reference to weapon type string
 
+ public:
+  Weapon(std::string type);
+  ~Weapon();
+  std::string getType();
+  void setType(std::string type);
 };
 
 #endif //CPP01_EX03_WEAPON_H_
