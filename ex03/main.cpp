610917d9e1d83b43dd7261cf2e5f92a2b3f484c5
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-07 21:35:11 +0900

    feat: implement main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,11 +6,26 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 20:40:23 by yena              #+#    #+#             */
-/*   Updated: 2023/06/07 20:43:04 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/07 21:14:57 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int main(void)
 {
+  {
+    Weapon club = Weapon("crude spiked club");
+    HumanA bob("Bob", club);
+    bob.attack();
+    club.setType("some other type of club");
+    bob.attack();
+  }
+  {
+    Weapon club = Weapon("crude spiked club");
+    HumanB jim("Jim");
+    jim.setWeapon(club);
+    jim.attack();
+    club.setType("some other type of club");
+    jim.attack();
+  }
   return (0);
 }
