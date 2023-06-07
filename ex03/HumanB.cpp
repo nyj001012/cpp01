9d6940a0036670d5c6e8f7b2fa2bf378237d8570
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-07 21:25:16 +0900

    feat: implement HumanB class

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,8 +6,32 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 20:39:58 by yena              #+#    #+#             */
-/*   Updated: 2023/06/07 20:40:07 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/07 21:25:05 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) {
+ this->name = name;
+ this->_weapon = NULL;
+}
+
+HumanB(const HumanB &object) {
+
+}
+HumanB &operator=(const HumanB &object) {
+
+}
+
+HumanB::~HumanB(void) {
+
+}
+
+/**
+ * @brief Set the Weapon object.
+ */
+void HumanB::attack(void) {
+  std::cout << this->_name << "attacks with his "
+            << this->_weapon.getType() << std::endl;
+}
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,15 +6,27 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 20:39:58 by yena              #+#    #+#             */
-/*   Updated: 2023/06/07 20:40:11 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/07 21:22:04 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CPP01_EX03_HUMANB_HPP_
 #define CPP01_EX03_HUMANB_HPP_
 
+/**
+ * @brief HumanB class
+ */
 class HumanB {
+ private:
+  std::string _name; ///< HumanB's name
+  Weapon &_weapon; ///< HumanB's weapon
 
-};
+ public:
+  HumanB(std::string name);
+  HumanB(const HumanB &object);
+  HumanB &operator=(const HumanB &object);
+  ~HumanB(void);
+  void attack(void);
+}
 
 #endif //CPP01_EX03_HUMANB_HPP_
