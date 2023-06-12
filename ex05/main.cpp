0f44c35dd52a4b1e57b042b004fd0e0a9736d478
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-12 20:07:00 +0900

    feat: implement ex05

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,9 +6,21 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 20:40:23 by yena              #+#    #+#             */
-/*   Updated: 2023/06/12 19:05:56 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/12 19:49:14 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char *argv[]) {
+#include "Karen.hpp"
+
+int main(void) {
+  std::string level;
+  Karen karen;
+
+  while (1) {
+    std::cout << "Enter log level: ";
+    std::getline(std::cin, level);
+    if (level == "EXIT" || level == "")
+      break;
+    karen.complain(level);
+  }
 }
